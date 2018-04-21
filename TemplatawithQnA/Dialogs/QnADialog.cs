07432cc4fd3f813a6d0cd3fec19c49674fb09c60
Manuel Sánchez <Manuss20@gmail.com>
2018-04-21 11:39:40 +0200

    Init Commit

---
+++ TemplatawithQnA/Dialogs/QnADialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Configuration;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+
+namespace TemplatewithQnA.Dialogs
+{
+    [Serializable]
+    public class QnaDialog : QnAMakerDialog
+    {
+        public QnaDialog() : base(new QnAMakerService(new
+            QnAMakerAttribute(ConfigurationManager.AppSettings["QnaSubscriptionKey"],
+            ConfigurationManager.AppSettings["QnaKnowledgebaseId"], "Sorry, I couldn't find an answer for that", 0.5)))
+        {
+        }
+    }
+}
---
+++ TemplatawithQnA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TemplatewithQnA.WebApiApplication" Language="C#" %>
